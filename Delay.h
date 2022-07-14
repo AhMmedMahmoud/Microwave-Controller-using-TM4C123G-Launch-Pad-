89c54a1d13eabbd46a9799fc8269c3e357e2e16d
Ahmed Mahmoud <104006521+AhMmedMahmoud@users.noreply.github.com>
2022-07-14 05:18:08 +0200

    Delay header file

---
+++ Delay.h
@@ -0,0 +1,7 @@
+#ifndef Delay_H_INCLUDED
+#define Delay_H_INCLUDED
+
+void delay_milli(int n);                // make delay n mile second
+void delay_micro(int n);                // make delay n micro second
+
+#endif 
